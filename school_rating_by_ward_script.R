d22323fb43dd4caf1aae03371aa98cd8a66ea567
xiang_li <xiang_li@v1020-wn-2-1.campus-dynamic.uwaterloo.ca>
2018-10-26 15:32:01 -0400

    added school ratings by ward data

---
+++ school_rating_by_ward_script.R
@@ -0,0 +1,20 @@
+# Creating new dataframe
+schooldf <- data.frame(School_Profile_Information$School_ID,
+                       School_Profile_Information$Short_Name,
+                       School_Profile_Information$Overall_Rating,
+                       School_Profile_Information$Rating_Status,
+                       School_Locations$WARD_15)
+names(schooldf) <- c('id', 'name', 'rating', 'status', 'ward')
+
+# Converting ratings to numbers
+schooldf[, 'rating'] = toupper(schooldf[, 'rating'])
+schooldf$rating_num <- NA
+schooldf$rating_num[schooldf$rating=='INABILITY TO RATE'] <- -1
+schooldf$rating_num[is.na(schooldf$rating)] <- -1
+schooldf$rating_num[schooldf$rating=='LEVEL 3'] <- 1
+schooldf$rating_num[schooldf$rating=='LEVEL 2'] <- 2
+schooldf$rating_num[schooldf$rating=='LEVEL 2+'] <- 3
+schooldf$rating_num[schooldf$rating=='LEVEL 1'] <- 4
+schooldf$rating_num[schooldf$rating=='LEVEL 1+'] <- 5
+
+# cat("\014") 
