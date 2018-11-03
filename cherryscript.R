abeadc30fde9cc78d7fafac5c8565fef22d6ec98
Cherry Zhang <shadowingphantom@gmail.com>
2018-11-03 15:33:12 -0400

    added totalParkAreaByCommunityArea

--- cherryscript.R
+++ cherryscript.R
@@ -4,15 +4,38 @@
 library(rgdal)
 library(sp)
 library(dplyr)
+library(sf)
+library(tidyverse)
+library(raster)
 
 chicagoparks <- readOGR('4A/MSCI 446/R/chicagoparksshapefile2', 'geo_export_287c1e81-adfc-4076-bbd4-7ac4b1ca62c2')
+chicagocommunityareas <- readOGR('4A/MSCI 446/R/communityareashapefile', 'geo_export_f2c553e7-eb62-4773-9655-8037a1bdd109')
 
-chicagoparksdf <- data.frame(c(chicagoparks$ward), c(chicagoparks$acres))
-names(chicagoparksdf) <- c("ward", "acres")
+#TEST TO SEE HOW TO ACTUALLY GET INTERSECTION AND AREA OF INTERSECTION
+horanpark <- chicagoparks[which(chicagoparks@data$park=="HORAN (ALBERT)"),]
+eastgarfield <- chicagocommunityareas[which(chicagocommunityareas@data$community=="EAST GARFIELD PARK"),]
+Intersects <- raster::intersect(horanpark, eastgarfield)
+area(Intersects)
 
-parkacresbyward <- aggregate(.~ward, data=chicagoparksdf, mean)
+#RUN THIS CODE FOR TOTAL PARK AREA FOR EACH COMMUNITY AREA
+totalParkAreaForCommunityAreas <- rep(0, nrow(chicagocommunityareas))
+for(i in 1:nrow(chicagocommunityareas)) {
+  totalArea <- 0
+  for (j in 1:nrow(chicagoparks)) {
+    intersect <- raster::intersect(chicagocommunityareas[i, ], chicagoparks[j, ])
+    if (!is.null(intersect)) {
+      totalArea <- totalArea + area(intersect)
+    }
+  }
+  totalParkAreaForCommunityAreas[i] <- totalArea
+}
 
-plot(chicagoparks)
+
+
+#OLD CODE: CHICAGO PARKS BY WARDS
+#chicagoparksdf <- data.frame(c(chicagoparks$ward), c(chicagoparks$acres))
+#names(chicagoparksdf) <- c("ward", "acres")
+#parkacresbyward <- aggregate(.~ward, data=chicagoparksdf, mean)
 
 ##########################################
 #poverty & race by community area
