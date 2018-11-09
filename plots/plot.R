7550183417725afa1972f23347c715aeec7707b0
xiang_li <xiang_li@v1020-wn-16-146.campus-dynamic.uwaterloo.ca>
2018-11-09 14:13:59 -0500

    added plots

--- plots/plot.R
+++ plots/plot.R
@@ -1,39 +1,72 @@
 # graphics.off()
+# cat('\014')
+
 explanatory <- data.frame(predTable$communityAreaNum,
                           predTable$percentViolentCrimePer1000Population,
                           predTable$avgSchoolRating,
                           predTable$avgSSLRating,
                           predTable$totalParkArea,
                           predTable$numHospitals,
-                          predTable$teenMomRate,
-                          predTable$infantMortalityRate,
-                          predTable$hispanic,
-                          predTable$black,
-                          predTable$white,
-                          predTable$asian,
-                          predTable$other,
-                          predTable$percentChildrenInPov)
-names(explanatory) <- c('community', 'percent_violent_crime_per_1000_population',
-                        'avg_school_rating', 'avg_SSL', 'total_park_area',
-                        'num_hospitals', 'teen_mom_rate', 'infant_mortality_rate',
-                        'race_hispanic', 'race_black', 'race_white',
-                        'race_asian', 'race_other', 'percent_children_in_poverty')
-col_names <- colnames(explanatory)
+                          100*predTable$teenMomRate,
+                          100*predTable$infantMortalityRate,
+                          100*predTable$hispanic,
+                          100*predTable$black,
+                          100*predTable$white,
+                          100*predTable$asian,
+                          100*predTable$other,
+                          100*predTable$percentChildrenInPov)
+names(explanatory) <- c('community', 'number_of_violent_crimes_per_1000_population',
+                        'Average_School_Rating', 'Normalized_Average_SSL', 'Total_Park_Area_(km2)',
+                        'Number_of_Hospitals', 'Teen_Mom_Rate_(%)', 'Infant_Mortality_Rate_(%)',
+                        'Percent_of_Hispanic_(%)', 'Percent_of_Black_(%)', 'Percent_of_White_(%)',
+                        'Percent_of_Asian_(%)', 'Percent_of_Other_Race_(%)', 'Percent_of_Children_in_Poverty_(%)')
 
 # normalize SSL (266.0711 - 304.1068)
-explanatory$avg_SSL <- (explanatory$avg_SSL-min(explanatory$avg_SSL))/(max(explanatory$avg_SSL) - min(explanatory$avg_SSL))
+explanatory$Normalized_Average_SSL <- (explanatory$Normalized_Average_SSL-min(explanatory$Normalized_Average_SSL))/(max(explanatory$Normalized_Average_SSL) - min(explanatory$Normalized_Average_SSL))
 
 # num_hospital to binary
-explanatory$num_hospital_greater_than_3 <- NA
-explanatory$num_hospital_greater_than_3 <- explanatory$num_hospitals >= 3
+explanatory$Whether_Community_Has_3_or_More_Hospitals <- NA
+explanatory$Whether_Community_Has_3_or_More_Hospitals <- explanatory$Number_of_Hospitals >= 3
 
+col_names <- colnames(explanatory)
+
+# plot everything
 for(i in 3:14) {
-  plot(explanatory[,i], explanatory$percent_violent_crime_per_1000_population,
-       xlab=col_names[i], ylab="% violent crimes per 1000 population", pch=17)
+  plot(explanatory[,i], explanatory$number_of_violent_crimes_per_1000_population,
+       main=paste('Violent Crime Rate V.S.', gsub( '\\s*\\([^\\)]+\\)', '', gsub('_', ' ', col_names[i]))),
+       xlab=gsub('_', ' ', col_names[i]), ylab='Number of Violent Crimes / 1000 Population', pch=18)
 }
 
-plot(explanatory[,15], explanatory$percent_violent_crime_per_1000_population,
-     xlab=col_names[15], ylab="% violent crimes per 1000 population", pch=18)
+plot(explanatory[,15], explanatory$number_of_violent_crimes_per_1000_population,
+     main=paste('Violent Crime Rate V.S. ', gsub( '\\s*\\([^\\)]+\\)', '', gsub('_', ' ', col_names[15]))),
+     xlab=gsub('_', ' ', col_names[15]), ylab='Number of Violent Crimes / 1000 Population', pch=18)
 axis(1, at=1:2, labels=c('FALSE', 'TRUE'))
 
+# corrolation
 # park hospital teen_mom_rate infant_mortality races(4) poverty_children
+
+# histograms
+hist(explanatory$number_of_violent_crimes_per_1000_population, 
+     col='grey',
+     main='Number of Violent Crimes / 1000 Population (Histogram)',
+     xlab='Number of Violent Crimes / 1000 Population', ylab='Number of Communities')
+
+for(i in 3:14) {
+  hist(explanatory[,i],
+       col='grey',
+       main=paste(gsub( '\\s*\\([^\\)]+\\)', '', gsub('_', ' ', col_names[i])), '(Histogram)'),
+       xlab=gsub('_', ' ', col_names[i]), ylab='Number of Communities')
+}
+
+# box plots
+boxplot(explanatory$number_of_violent_crimes_per_1000_population, data=explanatory,
+        col='grey',
+        main="Number of Violent Crimes / 1000 Population (Box Plot)",
+        xlab="Number of Violent Crimes / 1000 Population")
+
+for(i in 3:14) {
+  boxplot(explanatory[,i], data=explanatory,
+          col='grey',
+          main=paste(gsub( '\\s*\\([^\\)]+\\)', '', gsub('_', ' ', col_names[i])), '(Box Plot)'),
+          xlab=gsub('_', ' ', col_names[i]))
+}
