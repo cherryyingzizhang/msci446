f28bde62ec933f77c7714ad7a36480b8d0966b42
Cherry Zhang <shadowingphantom@gmail.com>
2018-11-10 12:17:49 -0500

    moved files around to make folder neater, finished majority of prediction script, added status update doc file

---
+++ plots/plot.R
@@ -0,0 +1,88 @@
+# graphics.off()
+# cat('\014')
+
+explanatory <- data.frame(predTable$communityAreaNum,
+                          predTable$percentViolentCrimePer1000Population,
+                          predTable$avgSchoolRating,
+                          predTable$avgSSLRating,
+                          predTable$totalParkArea,
+                          predTable$numHospitals,
+                          predTable$teenMomRate,
+                          predTable$infantMortalityRate,
+                          100*predTable$hispanic,
+                          100*predTable$black,
+                          100*predTable$white,
+                          100*predTable$asian,
+                          100*predTable$other,
+                          100*predTable$percentChildrenInPov)
+names(explanatory) <- c('community', 'number_of_violent_crimes_per_1000_population',
+                        'Average_School_Rating', 'Normalized_Average_SSL', 'Total_Park_Area_(m2)',
+                        'Number_of_Hospitals', 'Number_of_Teen_Moms_/_1000_Female_Teenagers', 'Number_of_Infant_Mortality_/_1000_Live_Births',
+                        'Percent_of_Hispanic_(%)', 'Percent_of_Black_(%)', 'Percent_of_White_(%)',
+                        'Percent_of_Asian_(%)', 'Percent_of_Other_Race_(%)', 'Percent_of_Children_in_Poverty_(%)')
+
+# normalize SSL (266.0711 - 304.1068)
+explanatory$Normalized_Average_SSL <- (explanatory$Normalized_Average_SSL-min(explanatory$Normalized_Average_SSL))/(max(explanatory$Normalized_Average_SSL) - min(explanatory$Normalized_Average_SSL))
+
+# num_hospital to binary
+explanatory$Whether_Community_Has_3_or_More_Hospitals <- NA
+explanatory$Whether_Community_Has_3_or_More_Hospitals <- explanatory$Number_of_Hospitals >= 3
+
+col_names <- colnames(explanatory)
+
+# plot everything
+for(i in 3:14) {
+  plot(explanatory[,i], explanatory$number_of_violent_crimes_per_1000_population,
+       main=paste('Violent Crime Rate V.S.', gsub( '\\s*\\([^\\)]+\\)', '', gsub('_', ' ', col_names[i]))),
+       xlab=gsub('_', ' ', col_names[i]), ylab='Number of Violent Crimes / 1000 Population', pch=18)
+}
+
+plot(explanatory[,15], explanatory$number_of_violent_crimes_per_1000_population,
+     main=paste('Violent Crime Rate V.S. ', gsub( '\\s*\\([^\\)]+\\)', '', gsub('_', ' ', col_names[15]))),
+     xlab=gsub('_', ' ', col_names[15]), ylab='Number of Violent Crimes / 1000 Population', pch=18)
+axis(1, at=1:2, labels=c('FALSE', 'TRUE'))
+
+# correlation
+# park hospital teen_mom_rate infant_mortality races(4) poverty_children
+
+# histograms
+hist(explanatory$number_of_violent_crimes_per_1000_population, 
+     col='grey',
+     main='Number of Violent Crimes / 1000 Population (Histogram)',
+     xlab='Number of Violent Crimes / 1000 Population', ylab='Number of Communities')
+
+for(i in 3:14) {
+  hist(explanatory[,i],
+       col='grey',
+       main=paste(gsub( '\\s*\\([^\\)]+\\)', '', gsub('_', ' ', col_names[i])), '(Histogram)'),
+       xlab=gsub('_', ' ', col_names[i]), ylab='Number of Communities')
+}
+
+# box plots
+boxplot(explanatory$number_of_violent_crimes_per_1000_population, data=explanatory,
+        col='grey',
+        main="Number of Violent Crimes / 1000 Population (Box Plot)",
+        xlab="Number of Violent Crimes / 1000 Population")
+
+for(i in 3:14) {
+  boxplot(explanatory[,i], data=explanatory,
+          col='grey',
+          main=paste(gsub( '\\s*\\([^\\)]+\\)', '', gsub('_', ' ', col_names[i])), '(Box Plot)'),
+          xlab=gsub('_', ' ', col_names[i]))
+}
+
+# replot park area with supercript
+plot(explanatory$`Total_Park_Area_(m2)`, explanatory$number_of_violent_crimes_per_1000_population,
+     main='Violent Crime Rate V.S. Total Park Area',
+     xlab=expression('Total Park Area' ~ (m^{2})), ylab='Number of Violent Crimes / 1000 Population', pch=18)
+
+hist(explanatory$`Total_Park_Area_(m2)`,
+     col='grey',
+     main='Total Park Area (Histogram)',
+     xlab=expression('Total Park Area' ~ (m^{2})), ylab='Number of Communities')
+
+boxplot(explanatory$`Total_Park_Area_(m2)`, data=explanatory,
+        col='grey',
+        main='Total Park Area (Box Plot)',
+        xlab=expression('Total Park Area' ~ (m^{2})))
+
--- violent_crimes_in_community.R
+++ violent_crimes_in_community.R
@@ -38,10 +38,10 @@ count[is.na(count)] <- 0
 population <- data.frame(population_chicago$GeogKey,
                         population_chicago$Geog,
                         population_chicago$`Total Population`)
-names(population) <- c('community', 'community name', 'population')
+names(population) <- c('community', 'community name', 'population(2010)')
 
 # sum violent crimes and total crimes
-sum_crime <- data.frame(count$community)
+
 names(sum_crime) <- c('community')
 sum_crime$violent_crime <- NA
 sum_crime$total_crime <- NA
@@ -49,10 +49,56 @@ sum_crime$violent_crime <- rowSums(count[, c('ASSAULT', 'BATTERY', 'CRIM SEXUAL 
                                              'HOMICIDE', 'KIDNAPPING',
                                              'VIOLENT OFFENSE INVOLVING CHILDREN',
                                              'PUBLIC PEACE VIOLATION', 'RITUALISM',
-                                             'ROBBERY', 'ROBBERY', 'SEX OFFENSE',
-                                             'WEAPONS VIOLATION')])
+                                             'ROBBERY', 'SEX OFFENSE', 'WEAPONS VIOLATION')])
 sum_crime$total_crime <- rowSums(count[, !(colnames(count) == "community")])
 
 #merge sum_crime and population
 sum_crime <- merge(x = sum_crime, y = population, by.x = 'community', by.y = 'community', all = TRUE)
 sum_crime <- sum_crime[, c(1, 4, 5, 2, 3)]
+
+#############################################
+
+# location description
+violent_crime <- crime[(crime$type=='ASSAULT'
+                      | crime$type=='BATTERY'
+                      | crime$type=='CRIM SEXUAL ASSAULT'
+                      | crime$type=='HOMICIDE'
+                      | crime$type=='KIDNAPPING'
+                      | crime$type=='VIOLENT OFFENSE INVOLVING CHILDREN'
+                      | crime$type=='PUBLIC PEACE VIOLATION'
+                      | crime$type=='RITUALISM'
+                      | crime$type=='ROBBERY'
+                      | crime$type=='SEX OFFENSE'
+                      | crime$type=='WEAPONS VIOLATION'),]
+
+# generating counts
+location_description <- data.frame(crime$location, crime$community)
+names(location_description) <- c('location', 'community')
+location_description <- location_description[location_description$location != '',]
+
+count_location_description <- ddply(location_description, .(location_description$community, location_description$location), nrow)
+names(count_location_description) <- c("community", "location", "count")
+
+count_loc <- spread(count_location_description, key = location, value = count)
+count_loc[is.na(count_loc)] <- 0
+
+# get most frequent location for each community
+# NOT using location description as one of the explanatory variables
+# near-zero variance predictors are not helpful for predicting
+colnames(count_loc)[max.col(count_loc,ties.method="first")]
+
+# output results
+
+# [1] "STREET"           "STREET"           "STREET"           "STREET"           "STREET"           "STREET"          
+# [7] "STREET"           "STREET"           "RESIDENCE"        "RESIDENCE"        "STREET"           "RESIDENCE"       
+# [13] "STREET"           "STREET"           "STREET"           "STREET"           "RESIDENCE"        "RESIDENCE"       
+# [19] "STREET"           "STREET"           "STREET"           "STREET"           "STREET"           "STREET"          
+# [25] "STREET"           "STREET"           "STREET"           "STREET"           "STREET"           "STREET"          
+# [31] "STREET"           "STREET"           "STREET"           "STREET"           "STREET"           "STREET"          
+# [37] "STREET"           "STREET"           "STREET"           "STREET"           "STREET"           "STREET"          
+# [43] "APARTMENT"        "STREET"           "RESIDENCE"        "STREET"           "RESIDENCE"        "STREET"          
+# [49] "RESIDENCE"        "RESIDENCE"        "RESIDENCE"        "STREET"           "RESIDENCE"        "RESIDENCE"       
+# [55] "RESIDENCE"        "STREET"           "STREET"           "STREET"           "STREET"           "STREET"          
+# [61] "STREET"           "STREET"           "STREET"           "RESIDENCE"        "RESIDENCE"        "STREET"          
+# [67] "STREET"           "STREET"           "STREET"           "RESIDENCE"        "STREET"           "RESIDENCE"       
+# [73] "RESIDENCE"        "RESIDENCE"        "RESIDENCE"        "AIRPORT/AIRCRAFT" "STREET"  
